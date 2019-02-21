1743d2e0f441394eed6ed377f3749482f0733353
Your Name <surya.pandu315@gmail.com>
2019-02-21 19:04:10 +0530

    Initial commit

---
+++ test_storage.py
@@ -0,0 +1,14 @@
+import tempfile
+
+from pgbackup import storage
+
+def test_storing_file_locally():
+
+    infile = tempfile.TemporaryFile('r+b')
+    infile.write(b"Testing")
+    infile.seek(0)
+
+    outfile = tempfile.NamedTemporaryFile(delete=False)
+    storage.local(infile, outfile)
+    with open(outfile.name, 'rb') as f:
+        assert f.read() == b"Testing"
